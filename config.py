efa0b7d50699614d9bc10f6568f1e1219be5f7a5
ZhoraBoom <81982476+ZhoraBoom@users.noreply.github.com>
2022-12-03 19:10:48 +0300

    Update and rename token.py to config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = '5913562494:AAE1L323FB8mFBpDgxRIhYZxVufZb_eaUx0'
+SVIN = 'CgACAgQAAxkBAAOAY4ibsLXEUh_QRPa15Re56ePBN54AAr8CAAJlawxTEDIH0Ngu3UkrBA'
+instructions = "Вызвав команду request ты сможешь: либо поддержать своего фаворита, либо узнать статистику встреч сборной России со сборной, которая тебя интересует."
